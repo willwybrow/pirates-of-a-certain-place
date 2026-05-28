e7df2ac408750b493f5cd0ce23e7699cb7f2c4ee
Will Wybrow <will@willwybrow.com>
2026-05-28 12:49:58 +0100

    Minor refactor of main

--- core/src/main/java/dev/wycor/pirates/Main.java
+++ core/src/main/java/dev/wycor/pirates/Main.java
@@ -5,13 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import dev.wycor.pirates.geometry.Direction;
 import dev.wycor.pirates.geometry.Hex;
-
-import static dev.wycor.pirates.SeaEvent.KRAKEN;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
@@ -19,8 +16,6 @@ public class Main extends ApplicationAdapter {
     private static final float WORLD_HEIGHT = 10f;
     private static final float HEX_WIDTH = 1f;
     private static final float HEX_HEIGHT = 1f;
-    private static final float SQRT_3 = (float)Math.sqrt(3);
-    private static final float SQRT_3_OVER_2 = SQRT_3/2f;
 
     private SpriteBatch batch;
     private FitViewport viewport;
@@ -45,33 +40,18 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.16f/2f, 0.5f/2f, 0.725f/2f, 1f); // rgb(41, 128, 185)
+        ScreenUtils.clear(0.08f, 0.25f, 0.3625f, 1f);
 
         viewport.apply();
         batch.setProjectionMatrix(viewport.getCamera().combined);
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
-            sea.go(Direction.NORTHEAST);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
-            sea.go(Direction.EAST);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.X)) {
-            sea.go(Direction.SOUTHEAST);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.Z)) {
-            sea.go(Direction.SOUTHWEST);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {
-            sea.go(Direction.WEST);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
-            sea.go(Direction.NORTHWEST);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
-            sea.whatsAt(sea.currentPosition()).complete();
-        }
+        input();
 
+        draw(batch);
+
+    }
+
+    private void draw(SpriteBatch batch) {
         batch.begin();
         sea.walkTheSpiral((int)Math.max(WORLD_HEIGHT, WORLD_WIDTH)).forEach(exploredHex -> {
             batch.draw(seaHexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
@@ -93,6 +73,30 @@ public class Main extends ApplicationAdapter {
         batch.end();
     }
 
+    private void input() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
+            sea.go(Direction.NORTHEAST);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
+            sea.go(Direction.EAST);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.X)) {
+            sea.go(Direction.SOUTHEAST);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.Z)) {
+            sea.go(Direction.SOUTHWEST);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {
+            sea.go(Direction.WEST);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+            sea.go(Direction.NORTHWEST);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
+            sea.whatsAt(sea.currentPosition()).complete();
+        }
+    }
+
 
     @Override
     public void resize(int width, int height) {
@@ -107,13 +111,9 @@ public class Main extends ApplicationAdapter {
 
     public float boundingBoxX(Hex hex) {
         return (WORLD_WIDTH/2f - HEX_WIDTH) + hex.q() + 1/2f * hex.r();
-        // return (WORLD_WIDTH/2f - HEX_WIDTH) + (SQRT_3 * hex.q() + SQRT_3_OVER_2 * hex.r());
-        // return (WORLD_WIDTH/2f - HEX_WIDTH) + hex.q() + (hex.r() % 2) * (HEX_WIDTH / 2f);
     }
 
     public float boundingBoxY(Hex hex) {
         return ((WORLD_HEIGHT/2f - HEX_HEIGHT)) + 3/4f * -hex.r();
-//        return ((WORLD_HEIGHT/2f - HEX_HEIGHT)) + (1.5f * hex.r());
-        // return (WORLD_HEIGHT/2f - HEX_HEIGHT) + hex.r() * 0.7f;
     }
 }
