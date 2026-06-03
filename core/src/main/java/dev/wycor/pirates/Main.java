d58c5a866a5de8d1bd3ea8519401641761ef4be5
Will Wybrow <will@willwybrow.com>
2026-06-03 20:31:49 +0100

    make a hex drawing function

--- core/src/main/java/dev/wycor/pirates/Main.java
+++ core/src/main/java/dev/wycor/pirates/Main.java
@@ -17,6 +17,7 @@ import dev.wycor.pirates.geometry.Hex;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    private static final float FLOATS_PER_PIXEL = 0.005f;
     public static final float SIXTEEN_PIXELS = 0.08f;
     public static final float THIRTY_TWO_PIXELS = 0.16f;
     private static final float SCREEN_WIDTH = 1.6f;
@@ -55,7 +56,7 @@ public class Main extends ApplicationAdapter {
         islandTexture = new Texture("island_empty_1x_32.png");
         stockedIslandTexture = new Texture("island_stocked_1x_32.png");
         fogOfWarTexture = new Texture("unexplored_hex_32.png");
-        shipTexture = new Texture("hero_ship_1x_32.png");
+        shipTexture = new Texture("heroic_pirate_ship_16.png");
     }
 
     @Override
@@ -82,32 +83,40 @@ public class Main extends ApplicationAdapter {
 
         batch.begin();
         sea.walkTheSpiral(13).forEach(exploredHex -> {
-            batch.draw(seaHexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+            // batch.draw(seaHexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+            drawAtHex(seaHexture, exploredHex);
             SeaTile whatsHere = sea.whatsAt(exploredHex);
             if (!whatsHere.isExplored()) {
-                batch.draw(fogOfWarTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                // batch.draw(fogOfWarTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                drawAtHex(fogOfWarTexture, exploredHex);
             } else if (!whatsHere.isCompleted()) {
                 switch (whatsHere.pendingEvent()) {
                     case ISLAND:
-                        batch.draw(stockedIslandTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        // batch.draw(stockedIslandTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        drawAtHex(stockedIslandTexture, exploredHex);
                         break;
                     case KRAKEN:
-                        batch.draw(krakenTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        // batch.draw(krakenTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        drawAtHex(krakenTexture, exploredHex);
                         break;
                     case SQUID:
-                        batch.draw(squidTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        // batch.draw(squidTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        drawAtHex(squidTexture, exploredHex);
                         break;
                     case GHOST:
-                        batch.draw(ghostTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        // batch.draw(ghostTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                        drawAtHex(ghostTexture, exploredHex);
                         break;
                 }
             } else {
                 if (whatsHere.completedEvent() == SeaEvent.ISLAND) {
-                    batch.draw(islandTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                    // batch.draw(islandTexture, boundingBoxX(exploredHex), boundingBoxY(exploredHex), HEX_WIDTH, HEX_HEIGHT);
+                    drawAtHex(islandTexture, exploredHex);
                 }
             }
         });
-        batch.draw(shipTexture, boundingBoxX(sea.currentPosition()), boundingBoxY(sea.currentPosition()), HEX_WIDTH, HEX_HEIGHT);
+        // batch.draw(shipTexture, boundingBoxX(sea.currentPosition()), boundingBoxY(sea.currentPosition()), HEX_WIDTH, HEX_HEIGHT);
+        drawAtHex(shipTexture, sea.currentPosition());
         batch.end();
     }
 
@@ -163,5 +172,18 @@ public class Main extends ApplicationAdapter {
 
     public float boundingBoxY(Hex hex) {
         return (3/4f * -hex.r()) * HEX_HEIGHT;
+    }
+
+    private void drawAtHex(Texture texture, Hex drawAt) {
+        float width = texture.getWidth() * FLOATS_PER_PIXEL;
+        float height = texture.getHeight() * FLOATS_PER_PIXEL;
+
+        float centreX = ((drawAt.q() + 1/2f * drawAt.r()) * HEX_WIDTH) + (HEX_WIDTH / 2);
+        float centreY = ((3/4f * -drawAt.r()) * HEX_HEIGHT) + (HEX_HEIGHT / 2);
+
+        float bottomX = centreX - (width / 2f);
+        float bottomY = centreY - (height / 2f);
+
+        batch.draw(texture, bottomX, bottomY, width, height);
     }
 }
